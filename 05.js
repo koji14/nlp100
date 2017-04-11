4245a8f04a6d2a4bb17c54fda760aab9e938566f
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-11 16:31:53 +0900

    コメント追加した

--- 05.js
+++ 05.js
@@ -4,12 +4,16 @@ let w_gram = [];
 let c_gram = [];
 
 function n_gram(params) {
+    // 単語ごと分割
     let word = params.split(" ");
+    // ” ”を無視して1文字ごと分割
     let char = params.replace(/ /g,'').split("");
+    // 単語bi-gram
     for (let i = 0; i < word.length -1; i++) {
         let array = [word[i], word[i+1]]
         w_gram.push(array);
     }
+    // 文字bi-gram
     for (var i = 0; i < char.length - 1; i++) {
         let array = [char[i], char[i + 1]]
         c_gram.push(array);   
