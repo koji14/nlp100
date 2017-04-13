e32ca70a80d301cc0998b9f5e31e245638ac967a
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-13 14:49:44 +0900

    コメント追加

--- 09.js
+++ 09.js
@@ -2,8 +2,11 @@ let str = "I couldn't believe that I could actually understand what I was readin
 let answer = "";
 str.forEach(function(element) {
     if(element.length > 4){
+        // シャッフル用関数
         var shuffle = function() {return Math.random()-.5};
         // console.log(element.charAt(0) + element.slice(1,-1).split("").sort(shuffle).join("") + element.slice(-1));
+        
+        // 一文字目　+　sliceで各単語の先頭と末尾の文字を抽出してランダムに入れ替える　+　単語の最後の文字
         answer += element.charAt(0) + element.slice(1,-1).split("").sort(shuffle).join("") + element.slice(-1) + " ";
     }
     else{
