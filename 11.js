9696128c89f962add29983c1ef8ccd7214f09b53
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-14 13:38:29 +0900

    アロー関数に変更

--- 11.js
+++ 11.js
@@ -1,5 +1,5 @@
 let fs = require('fs');
-fs.readFile('./hightemp.txt', 'utf8', function(err, text){
+fs.readFile('./hightemp.txt', 'utf8', (err, text) => {
     if (err) throw err;
     text.split('\n').forEach( (element) => {
         console.log(element.replace(/\t/g, " "));
