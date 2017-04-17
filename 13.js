205b6ab2c4ad53c720663c7205863af1e84225a8
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-17 19:11:43 +0900

    コメントを追加

--- 13.js
+++ 13.js
@@ -3,12 +3,15 @@ let fs = require('fs');
 let col1 = [];
 let col2 = [];
 let col3 = [];
+// col1 ファイル読み込みし改行コードで分割
 col1 = fs.readFileSync('./col1.txt', 'utf8').split('\r\n').slice(1, -1);
 // console.log(col1);
 
+// col2 ファイル読み込みし改行コードで分割
 col2 = fs.readFileSync('./col2.txt', 'utf8').split('\r\n').slice(1, -1);
 // console.log(col2);
 
+// col1を1つごと取り出して col1 \t col2 \n  
 for (var i = 0; i < col1.length; i++) {
     col3 += col1[i] + '\t' + col2[i] + '\n'; 
 }
