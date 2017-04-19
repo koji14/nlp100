9587fca8c44af8ac33f673191de12c64c792352d
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-19 13:45:16 +0900

    add 15.js

---
+++ 15.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let  = '';
+
+if (process.argv.length <= 3 ){
+    console.log("引数が足りない");
+    process.exit(1);
+}
+
+let num = process.argv[3];
+file = fs.readFileSync(process.argv[2], 'utf-8').split('\n');
+file = file.slice((file.length-1) - num, -1);
+file.forEach(function(element) {
+    console.log(element);
+}, this);
