eafe4abcae0fa5b9baa6a4b90b2862481afec78b
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-19 13:53:02 +0900

    コメント追加

--- 15.js
+++ 15.js
@@ -1,13 +1,14 @@
 let fs = require('fs');
 let  = '';
 
+// 引数チェック
 if (process.argv.length <= 3 ){
     console.log("引数が足りない");
     process.exit(1);
 }
-
-let num = process.argv[3];
+// ファイル読み込み　1行分割
 file = fs.readFileSync(process.argv[2], 'utf-8').split('\n');
-file.slice((file.length-1) - num, -1).forEach(function(element) {
+// スライスで下からｎ行
+file.slice((file.length-1) - process.argv[3], -1).forEach(function(element) {
     console.log(element);
 }, this);
