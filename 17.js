020297e42c091dbea30b85e6243069cb23ff6b30
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-25 14:27:48 +0900

    使っていない変数を削除

--- 17.js
+++ 17.js
@@ -1,16 +1,16 @@
 let fs = require('fs');
 
-let  file = '';
-let list = [];
-var X = new Set(list);
+var X = new Set("");
 // 引数チェック
 if (process.argv.length <= 2 ){
     console.log("引数が足りない");
     process.exit(1);
 }
 // ファイル読み込み　1行分割
-file = fs.readFileSync(process.argv[2], 'utf-8').split('\n').slice(1, -1);
-file.forEach(element => {
+fs.readFileSync(process.argv[2], 'utf-8')
+.split('\n')
+.slice(1, -1)
+.forEach(element => {
     X.add(element.split('\t')[0]);
 }, this);
 
