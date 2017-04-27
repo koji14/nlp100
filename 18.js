41d5817ea6a9ba906fc50e1368882b6a4a4e0738
koji14 <b1402764@planet.kanazawa-it.ac.jp>
2017-04-27 14:21:40 +0900

    コメント追加

--- 18.js
+++ 18.js
@@ -7,13 +7,13 @@ if (process.argv.length <= 2 ){
     console.log("引数が足りない");
     process.exit(1);
 }
-// ファイル読み込み　1行分割
-fs.readFileSync(process.argv[2], 'utf-8')
-.split('\n').slice(1, -1)
+// ファイル読み込み　1行分割 2次元配列に変換
+fs.readFileSync(process.argv[2], 'utf-8').split('\n').slice(1, -1)
 .forEach(element => {
     file.push(element.split('\t'));
 }, this);
 
+// ソート　関数定義で小数点でもソートできるように
 console.log(file.sort(function(a, b) {
   return (parseFloat(a[2]) < parseFloat(b[2])) ? 1 : -1;
 }));
